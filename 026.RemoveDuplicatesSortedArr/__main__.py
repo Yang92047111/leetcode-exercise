5fe86b8d5e5e8cac7d5812bde474258b46dd52a7
Yang <yang@YangdeMacBook-Pro.local>
2024-05-14 00:00:45 +0800

    feat: add the validLength function

--- 026.RemoveDuplicatesSortedArr/__main__.py
+++ 026.RemoveDuplicatesSortedArr/__main__.py
@@ -10,16 +10,22 @@ class Solution:
         return i + 1
     
 if __name__ == '__main__':
+    def validLength(nums, ans, expected):
+        assert(ans == len(expected))
+        for i in range(ans):
+            assert( nums[i] == expected[i])
+
     sol = Solution()
 
     test_case = {}
     result = {}
 
-    test_case['case1'] = [[1, 1, 2], 2]
-    test_case['case2'] = [[0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 5]
+    test_case['case1'] = [[1, 1, 2], 2, [1, 2]]
+    test_case['case2'] = [[0, 0, 1, 1, 1, 2, 2, 3, 3, 4], 5, [0, 1, 2, 3, 4]]
 
     for key, val in test_case.items():
         ans = sol.removeDuplicates(val[0])
+        validLength(val[0], ans, val[2])
         if ans == val[1]:
             result[key] = key + ' pass'
 
