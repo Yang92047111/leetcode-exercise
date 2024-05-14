a668431b9bc3158bf7961e9e8a3c6e0802bf8b32
Yang <yang@YangdeMBP.localdomain>
2024-05-14 15:48:12 +0800

    feat: add the 035 question solution with python3

---
+++ 035.SearchInsertPosition/__main__.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        for i in range(0, len(nums)):
+            if nums[i] >= target:
+                return i
+            
+        return len(nums)
+    
+if __name__ == '__main__':
+    sol = Solution()
+
+    test_case = {}
+    result = {}
+    
+    test_case['case1'] = [[1, 3, 5, 6], 5, 2]
+    test_case['case2'] = [[1, 3, 5, 6], 2, 1]
+    test_case['case2'] = [[1, 3, 5, 6], 7, 4]
+
+    for key, val in test_case.items():
+        ans = sol.searchInsert(val[0], val[1])
+        if ans == val[2]:
+            result[key] = key + ' pass'
+
+    print (result)
